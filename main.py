f99165d0ef917c489847b192bc036839d6d9ba74
Antoine Chen <102303534+Sachachen@users.noreply.github.com>
2025-02-11 14:45:41 +0000

    asciiart

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+"""code pour retourner une liste de tuple comprenant 
+les caracteres d une liste et leurs occurences"""
 #### Imports et définition des variables globales
-
-
+import sys
 #### Fonctions secondaires
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères
+     passée en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,14 +15,24 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
 
-    return [ ]
+    # votre code ici
+    car = [s[0]]
+    occ = [1]
+    n = len(s)
+    for i in range(1,n):
+        if s[i] == s[i-1]:
+            occ[-1] += 1
+        else :
+            car.append(s[i])
+            occ.append(1)
+    l = list(zip(car,occ,strict = True))
+    return l
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de caractères 
+    passée en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -28,20 +40,27 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    # cas de base
+    sys.setrecursionlimit(4000)
+    n = len(s)
+    # cas en fin de liste, donc liste vide
+    if not s :
+        return []
+    # cas de base, premier caractere
+    base = s[0]
+    c = 1
     # recherche nombre de caractères identiques au premier
-    # appel récursif
+    for i in range(1,n) :
+        if s[i] == base :
+            c += 1
+        else :
+            break
+    # on enleve la base de la liste avec replace par "" et puis appel récursif
+    return [(base, c)] + artcode_r(s[c:])
 
-    return []
-    
 
 #### Fonction principale
-
-
 def main():
+    """main function pour appel des secondaires"""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
